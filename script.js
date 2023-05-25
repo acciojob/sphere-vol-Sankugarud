bdb823a2adde43d181035c868b22f952a2a4411b
sanket <95356246+Sankugarud@users.noreply.github.com>
2023-05-26 02:16:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 function volume_sphere() {
-    let vl = document.getElementById("radius");
+    let v1 = document.getElementById("radius");
+	// alert(v1.value)
+	let cal = 4/3 * (3.14159 * v1.value);
 	let v2 = document.getElementById("volume")
-  
+	 v2.value= cal;
 } 
-
+	
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
